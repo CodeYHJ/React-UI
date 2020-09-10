4dcb5a8173eee0ae28e769d17ff8d0f08129487b
CodeYHJ <colgateyhj@gmail.com>
2020-09-10 17:02:41 +0800

    eslint:init

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  eslintIntegration: true,
-  printWidth: 120, //一行的字符数，如果超过会进行换行，默认为80
-  tabWidth: 4, //一个tab代表几个空格数，默认为2
-  semi: true,
-  endOfLine: 'lf',
-  singleQuote: true,
+    eslintIntegration: true,
+    printWidth: 120, //一行的字符数，如果超过会进行换行，默认为80
+    tabWidth: 2, //一个tab代表几个空格数，默认为2
+    semi: true,
+    endOfLine: 'lf',
+    singleQuote: true,
 };
