682ae89001207b449d7181f275af392d63e6671c
CodeYHJ <colgateyhj@gmail.com>
2020-09-10 17:51:36 +0800

    formatter: button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,62 +1,67 @@
 import React, { MouseEventHandler, HtmlHTMLAttributes, ReactNode } from 'react';
-import "./style/index.less"
-import { classPre } from '@com/utils';
-import { LoadingIcon } from "@com/Icon"
 
-type ButtonType = 'primary' | 'danger' | 'warn' | 'default' | 'success' | 'dashed'
+import './style/index.less';
+
+import { classPre } from '@com/utils';
+
+import { LoadingIcon } from '@com/Icon';
+
+type ButtonType = 'primary' | 'danger' | 'warn' | 'default' | 'success' | 'dashed';
 
 export interface ButtonProps extends HtmlHTMLAttributes<HTMLButtonElement> {
-    type?: ButtonType,
-    className?: string,
-    onClick?: MouseEventHandler,
-    disabled?: boolean,
-    pre?: ReactNode,
-    loading?: boolean
+  type?: ButtonType;
+  className?: string;
+  onClick?: MouseEventHandler;
+  disabled?: boolean;
+  pre?: ReactNode;
+  loading?: boolean;
 }
-const c = classPre('button')
+
+const c = classPre('button');
 
 const Button: React.SFC<ButtonProps> = (props) => {
-    const { type = 'default', className, disabled, onClick, pre, loading, ...other } = props
-    const defaultCls = [c(), c(type), className]
-    if (disabled) {
-        if (type === 'dashed') {
-            defaultCls.push(c('dashed-disabled'))
-        } else {
-            defaultCls.push(c('disabled'))
+  const { type = 'default', className, disabled, onClick, pre, loading, ...other } = props;
 
-        }
+  const defaultCls = [c(), c(type), className];
+
+  if (disabled) {
+    if (type === 'dashed') {
+      defaultCls.push(c('dashed-disabled'));
+    } else {
+      defaultCls.push(c('disabled'));
     }
-    const cls = defaultCls.filter(Boolean).join(' ')
-    const handleClick: MouseEventHandler = (e) => {
-        if (!disabled) {
-            onClick && onClick(e)
-        }
+  }
+
+  const cls = defaultCls.filter(Boolean).join(' ');
+
+  const handleClick: MouseEventHandler = (e) => {
+    if (!disabled && onClick) {
+      onClick(e);
     }
-    const loadingSvg = () => {
-        return (
-            <div className={c('loading')}>
-                <LoadingIcon />
-            </div>
-        )
+  };
+
+  const loadingSvg = () => (
+    <div className={c('loading')}>
+      <LoadingIcon />
+    </div>
+  );
+
+  const renderIcon = () => {
+    if (disabled) return null;
+
+    if (loading) {
+      return loadingSvg();
     }
-    const renderIcon = () => {
-        if (disabled) return null
-        if (loading) {
-            return loadingSvg()
-        } else {
-            return pre ? (
-                <div className={c('preIcon')}>
-                    {pre}
-                </div>
-            ) : null
-        }
-    }
-    return (
-        <button className={cls} onClick={handleClick} {...other}>
-            {renderIcon()}
-            <span>{props.children}</span>
-        </button>
-    );
-}
+
+    return pre ? <div className={c('preIcon')}>{pre}</div> : null;
+  };
+
+  return (
+    <button className={cls} onClick={handleClick} {...other}>
+      {renderIcon()}
+      <span>{props.children}</span>
+    </button>
+  );
+};
 
 export default Button;
