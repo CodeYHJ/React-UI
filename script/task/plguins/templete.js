582994989ff173b8ae4a8d8634ce619a4f487013
CodeYHJ <colgateyhj@gmail.com>
2020-09-11 12:02:56 +0800

    script: templete change

--- script/task/plguins/templete.js
+++ script/task/plguins/templete.js
@@ -1,5 +1,7 @@
 const generateIconInfoTemplete = (svgComponent, content) => {
   const str = `
+/* eslint-disable max-len */
+
 import { SvgInfo } from "../svgInfo"
 
 const ${svgComponent}: SvgInfo =  ${content};
@@ -10,25 +12,28 @@ export default  ${svgComponent};
 
   return str;
 };
+
 const greateSvgIconIndexTemplete = (content) => {
   const str = content;
+
   return str;
 };
+
 const generateIconIndexTemplete = () => {
   const str = `
 
   import I from './localIcon'
 
   import c from './createIcon'
-  
+
   export * from './svgIcon'
-  
+
   type IconType = typeof I & { createIcon: typeof c }
-  
+
   const Icon = I as IconType
-  
+
   Icon.createIcon = c
-  
+
   export default Icon
 
   `;
@@ -43,38 +48,41 @@ const generateIconBaseTemplete = (tsName, componentName) => {
 
   import info from '../iconInfo/${tsName}Info'
 
+  import { classPre } from '@com/utils';
+
   import "../style/index.less";
 
-  import { classPre } from '@com/utils';
-  
-  export interface ${componentName}Props extends SVGAttributes<SVGElement> {
-  
-  }
+  // eslint-disable-next-line prettier/prettier
+  export interface ${componentName}Props extends SVGAttributes<SVGElement> {}
+
   const c = classPre("svg")
-  
-  const ${componentName}: React.SFC<${componentName}Props> = (props) => {
-  
+
+  const ${componentName}: React.FunctionComponent<${componentName}Props> = (props) => {
+
       const { className, ...others } = props
-  
+
       const cls = [c(), className].filter(Boolean).join(" ")
 
-      const childrenList = info.path.map((d,index) => React.createElement('path', { d ,key:index}))
-  
+      const childrenList = info.path.map(d => React.createElement('path', { d ,key:info.attributes.key}))
+
       return React.createElement('svg', { ...info.attributes, className: cls, ...others }, childrenList)
   }
-  
+
   export default ${componentName};
 
   `;
 
   return str;
 };
+
 const generateIconExportTemplete = (iconName) => {
   const str = `
   export { default as ${iconName} } from "./${iconName}"\n
   `;
+
   return str;
 };
+
 module.exports = {
   generateIconInfoTemplete,
   generateIconIndexTemplete,
